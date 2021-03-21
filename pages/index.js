52286330527336cc11291cb33f2165ef571b09ff
Enk <enk@Enks-MacBook-Pro.local>
2021-03-21 15:19:00 +0800

    Version 1 grid, list, intro

--- pages/index.js
+++ pages/index.js
@@ -7,20 +7,67 @@ import {
   Media,
   Image,
   Card,
-} from 'react-bootstrap'
+} from "react-bootstrap";
 
-import MyNavbar from '../Component/my-navbar'
+import MyNavbar from "Component/my-navbar";
+import Intro from "Component/intro";
+import ListItem from "Component/list-item"
+import GridItem from "Component/grid-item";
 
 export default function Home() {
   return (
     <Container>
-      <div>
       <MyNavbar />
+      <div className="blog-detail-page">
+        <Row>
+          <Col md="12">
+            <Intro />
+          </Col>
+        </Row>
 
+        <hr />
+
+        <div className={`page-wrapper`}>
+          <Row className="mb-5">
+            <Col md="10">
+              <ListItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+          </Row>
+        </div>
       </div>
-    
-
+      <footer className="page-footer">
+        <div>
+          <a href="#">нүүр</a>
+          {" | "}
+          <a href="#">сургалт</a>
+          {" | "}
+          <a href="#">фэйсбүүк</a>
+        </div>
+      </footer>
     </Container>
-   
-  )
+  );
 }
